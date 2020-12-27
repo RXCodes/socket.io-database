c56f64ff7729371bbf85c8817204a047f4d919cc
RXCodes <61912060+RXCodes@users.noreply.github.com>
2020-12-27 06:18:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -230,6 +230,7 @@ io.on('connection', function(socket) {
       
       // set room data
       socket.join(roomJSON.code);
+      socket.joined = true;
       rooms[roomJSON.code] = roomJSON;
       socket.room = roomJSON.code;
       let colorData = [0,1,2,3,4,5,6,7,8,9,10,11];
