34e3da38c00cfd9781e70291e4a4f021fba3cb22
RXCodes <61912060+RXCodes@users.noreply.github.com>
2021-01-06 11:39:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var port = process.env.PORT || 3000;
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+var fetch = require('node-fetch');
 
 // initialize variables and functions
 var rooms = {};
