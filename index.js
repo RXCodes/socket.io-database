e7afffb6bba4d3d56921daffda77a3b3c4ecf3d2
RXCodes <61912060+RXCodes@users.noreply.github.com>
2021-01-08 05:18:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var port = process.env.PORT || 3000;
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+const https = require('https');
 
 // initialize variables and functions
 var data = {};
