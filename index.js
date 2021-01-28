3e39afa1d499eb703bae607c2681a8411e061ca7
RXCodes <61912060+RXCodes@users.noreply.github.com>
2021-01-27 22:38:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -157,10 +157,10 @@ var syncData = function() {
   io.emit('console log', "sync started.");
   io.emit('console log', leaderboard);
 
-  let packet = JSON.stringify({
+  let packet = {
     pw: "8043EBACC7CAE08DC1A09B2B5DF472B2D44A06EEE3AEA12B0E6FB66CB7839788",
     data: leaderboard
-  });
+  };
   
   let options = {
     hostname: 'botpixelgames.000webhostapp.com',
