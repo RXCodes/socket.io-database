d8b5224d71e905f5c7d3e57010e61616cba3f36b
RXCodes <61912060+RXCodes@users.noreply.github.com>
2021-02-05 00:32:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -529,7 +529,8 @@ var updateLeaderboard = function () {
       iterations++;
       let discord = displayNames[key];
       if (replays[discord] !== undefined) {
-        let replayData = replays[discord][targets[i]].split("*");
+        let replayData = replays[discord][targets[i]];
+        let replayData = replayData.split("*");
         if (replayData !== undefined) {
           leaderboard[targets[i]][key].time = levelTimes[targets[i]] - (replayData.length * 0.033333333);
           leaderboard[targets[i]][key].score = (levelTimes[targets[i]] - (replayData.length * 0.033333333)) * 4000 * levelWeights[targets[i]];
